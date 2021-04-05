6f268280ee2fd49c12d04be6c73f2ec5e87e4b11
Antek <antek.gawron@gmail.com>
2021-04-05 19:35:28 +0200

    about section

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css'
+const About = () => {
+  return (
+    <section className="about">
+      <h3>O nas</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quia ratione assumenda blanditiis iste dolorem officia culpa nobis eos consequatur natus facilis, quis mollitia laudantium fugit minima autem voluptatibus consectetur.</p>
+    </section>
+  );
+}
+
+export default About;
